a7914a9e3a65a0b12fd18b844ef8a60fedc60533
Ivan Mota <imotaaguil@miners.utep.edu>
2018-12-23 20:43:41 -0700

    Added app backend

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var socket = require("socket.io");
+
+var app = express();
+var server = app.listen(process.env.PORT || 8000);
+app.use(express.static("public"));
+
+console.log("Server Running");
+
+var io = socket(server);
+io.sockets.on("connection", newConnection);
+
+function newConnection(socket){
+	console.log("New Connection: " + socket.id);
+}
